594f7294950efa2316317d2d6016a29e74bd0bd7
An Tuo <antuo030605@gmail.com>
2026-01-01 12:15:38 +0800

    fix bug

--- _scripts/search.liquid.js
+++ _scripts/search.liquid.js
@@ -99,8 +99,8 @@ ninja.data = [
             },
           {%- endunless -%}
         },
-      {%- endfor -%}
         {%- endif -%}
+      {%- endfor -%}
     {%- endif -%}
   {%- endfor -%}
   {%endcomment%}
